9d0101ca6006c58a52c349e8cb0ec70ced479614
Gary van der Merwe <garyvdm@gmail.com>
2014-10-04 18:56:00 +0200

    svn-buildpackages cleaning

--- svn2git.py
+++ svn2git.py
@@ -3,6 +3,7 @@
 import os
 import shutil
 import subprocess
+import re
 
 
 SVN2GIT = 'svn2git'
@@ -81,6 +82,35 @@ def migrate():
     # TODO: identity-map
     subprocess.check_call((SVN2GIT, '--rules=rules.txt', '--stats',
                            '../python-modules'))
+
+def clean_svn_buildpackages_commits(gitdir):
+    """Rewrites svn-buildpackages so that the only have one parent."""
+
+    run = subprocess.check_output
+
+    base_git_args = ['git', '--git-dir={}'.format(gitdir)]
+    refs = run(base_git_args + ['show-ref', '--tags'])
+
+    is_svn_buildpackage = re.compile(b'\n\n\[svn-buildpackage\]', flags=re.DOTALL).search
+
+    for ref in refs.splitlines():
+        ref_sha, _, ref_name = ref.partition(b' ')
+        commit = run(base_git_args + ['cat-file', '-p', ref_sha])
+        if is_svn_buildpackage(commit):
+            first_parent = run(base_git_args + ['rev-list', ref_sha, '-n 2']).splitlines()[1]
+            r = b'^parent (?!' + first_parent + b').*?\n'
+            new_commit, _ = re.subn(r, b'', commit, flags=re.MULTILINE)
+
+            hash_object_proc = subprocess.Popen(base_git_args + ['hash-object', '-w', '-t', 'commit', '--stdin'],
+                                                stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            hash_object_proc.stdin.write(new_commit)
+            hash_object_proc.stdin.close()
+            hash_object_proc.wait()
+            new_commit_sha = hash_object_proc.stdout.read().strip()
+
+            run(base_git_args + ['update-ref',
+                                 '-m', 'svn-buildpackage multiple parent cleanup',
+                                 ref_name, new_commit_sha, ref_sha])
 
 
 def println(f, line=''):
